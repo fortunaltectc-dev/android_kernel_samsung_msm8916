9cd58858913f8c788e0a166fa821132bd0951cf3
Zvikomborero Vincent Zvikaramba <zvikovincent@gmail.com>
2017-07-17 21:07:27 +0200

    drivers/input/touch_disabler.c: Initial sysfs support
    Change-Id: Ie5d265da2feeb9df3132e8deb0d7db0f12c27aea

--- drivers/input/touch_disabler.c
+++ drivers/input/touch_disabler.c
@@ -16,11 +16,74 @@
 #include <linux/of.h>
 #include <linux/slab.h>
 
+#include <linux/printk.h>
+#include <linux/kobject.h>
+#include <linux/sysfs.h>
+#include <linux/init.h>
+#include <linux/fs.h>
+#include <linux/string.h>
+
 #include <linux/input.h>
 
 #include <linux/input/touch_disabler.h>
 
+static struct kobject *touch_disabler_kobject;
+
+static int enabled; /* enable (1) /disable (0) touch devices */
+static int mode;    /* driver mode, between auto (0) and manual (1) */
+
+#define MODE_AUTO "auto"
+#define MODE_MANUAL "manual"
+
 touch_disabler_data_t touch_disabler_data;
+
+static void _touch_disabler_set_touch(bool status);
+
+static ssize_t enabled_show(struct kobject *kobj, struct kobj_attribute *attr,
+		      char *buf)
+{
+	return sprintf(buf, "%d\n", enabled);
+}
+
+static ssize_t enabled_store(struct kobject *kobj, struct kobj_attribute *attr,
+		      const char *buf, size_t count)
+{
+	/* only set the variable if mode is set to manual */
+	if (mode) {
+		sscanf(buf, "%du", &enabled);
+		_touch_disabler_set_touch(enabled);
+	}
+
+	return count;
+}
+
+static ssize_t mode_show(struct kobject *kobj, struct kobj_attribute *attr,
+		      char *buf)
+{
+	if (mode) {
+		return sprintf(buf, "%s\n", MODE_MANUAL);
+	} else {
+		return sprintf(buf, "%s\n", MODE_AUTO);
+	}
+}
+
+static ssize_t mode_store(struct kobject *kobj, struct kobj_attribute *attr,
+		      const char *buf, size_t count)
+{
+	if (!strncmp(buf, MODE_MANUAL, strlen(MODE_MANUAL)) || !strncmp(buf, "1", 1)) {
+		mode = 1;
+	}
+	else if (!strncmp(buf, MODE_AUTO, strlen(MODE_AUTO)) || !strncmp(buf, "0", 1)) {
+		mode = 0;
+	}
+	return count;
+}
+
+static struct kobj_attribute mode_attribute =__ATTR(mode, 0660, mode_show,
+						   mode_store);
+
+static struct kobj_attribute enabled_attribute =__ATTR(enabled, 0660, enabled_show,
+						   enabled_store);
 
 /*
  * Touch key/panel enabler/disabler for samsung touch keys/panel drivers.
@@ -31,6 +94,17 @@ touch_disabler_data_t touch_disabler_data;
  */
 void touch_disabler_set_touch(bool status)
 {
+	/* let mdss trigger the enaling/disabling */
+	if (!mode)
+		_touch_disabler_set_touch(status);
+}
+
+static void _touch_disabler_set_touch(bool status)
+{
+
+	/* set the enabled variable */
+	enabled = status;
+
 	/* check if the struct has been initialised by the touch driver */
 	if (touch_disabler_data.ts_dev) {
 		if (status) {
@@ -63,12 +137,44 @@ void touch_disabler_set_touch(bool status)
 
 static int __init touch_disabler_init(void)
 {
+	int error = 0;
+
+	enabled = 0;
+	mode = 0;
+
 	touch_disabler_data.ts_dev = NULL;
 	touch_disabler_data.tk_dev = NULL;
+
+	pr_debug("%s: Module initialized successfully.\n", __func__);
+
+	touch_disabler_kobject = kobject_create_and_add("touch_disabler",
+						 kernel_kobj);
+	if(!touch_disabler_kobject)
+		return -ENOMEM;
+
+	error = sysfs_create_file(touch_disabler_kobject, &enabled_attribute.attr);
+
+	if (error) {
+		pr_debug("%s: failed to create /sys/kernel/touch_disabler/enabled\n", __func__);
+	}
+
+	error = sysfs_create_file(touch_disabler_kobject, &mode_attribute.attr);
+
+	if (error) {
+		pr_debug("%s: failed to create /sys/kernel/touch_disabler/mode\n", __func__);
+	}
+
 	return 0;
 }
 
+static void __exit touch_disabler_exit (void)
+{
+	pr_debug("%s: Module initialized successfully.\n", __func__);
+        kobject_put(touch_disabler_kobject);
+}
+
 module_init(touch_disabler_init);
+module_exit(touch_disabler_exit);
 
 MODULE_DESCRIPTION("Touch key/panel disabler for Samsung devices");
 MODULE_AUTHOR("Vladimir Bely <vlwwwwww@gmail.com>, Vincent Zvikaramba <zvikovincent@gmail.com>");
